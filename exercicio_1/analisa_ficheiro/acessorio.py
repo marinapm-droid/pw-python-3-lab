0e97db8c597a98407aa471df7fdbf94bd2787f0f
marinapm-droid <78854001+marinapm-droid@users.noreply.github.com>
2021-05-12 09:49:35 +0100

    Add files via upload

--- exercicio_1/analisa_ficheiro/acessorio.py
+++ exercicio_1/analisa_ficheiro/acessorio.py
@@ -1,15 +1,20 @@
 def pede_nome()->"String":
-    count=0
     while True:
         nome = input("Insira um nome de um ficheiro de texto, com extensão .txt\n")
-        for n in nome:
-            if n == ".":
-                split = nome.split(".")
-                if split[1] == "txt":
-                    return split[0]
-                    exit()            
+        try:
+            open(nome, "r")
+            for n in nome:
+                if n == ".":
+                    split = nome.split(".")
+                    if split[1] == "txt":
+                        return split[0]
+                        exit()      
+        except IOError:
+            print("nono")
 
 def gera_nome():
     nome_txt = pede_nome()
     return (nome_txt+".json")
+
+pede_nome()
 
